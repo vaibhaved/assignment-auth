1e657b6bcc7de269ea252360928f81be00c5e109
Vaibhav Sharma <vaibhavsharma062998@gmail.com>
2019-06-18 03:42:48 +0530

    updated package.json and server.js for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require('path')
 
 const app = express();
 
@@ -33,6 +34,13 @@ mongoose.connect(database, {
 app.use(passport.initialize());
 require("./config/passport") (passport);
 app.use("/api/users", users);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 //Configuring Port
 const port = process.env.PORT || 4000;
